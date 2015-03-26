5380807080ccdedc4cd281bf10252fa43d9e1a9f
Ben Mather <bwhmather@bwhmather.com>
2015-03-26 20:40:52 +0000

    add tests for skipping validation

--- jpake/tests/__init__.py
+++ jpake/tests/__init__.py
@@ -13,6 +13,36 @@ class JPAKETestCase(unittest.TestCase):
 
         alice.process_two(bob.two()), bob.process_two(alice.two())
 
+    def test_skip_verification_one(self):
+        secret = "hunter42"
+        alice = JPAKE(secret=secret, signer_id=b"alice")
+        bob = JPAKE(secret=secret, signer_id=b"bob")
+
+        bob_one = bob.one()
+
+        alice.process_one(gx3=bob_one['gx1'], gx4=bob_one['gx2'], verify=False)
+
+    def test_no_proofs_one(self):
+        secret = "hunter42"
+        alice = JPAKE(secret=secret, signer_id=b"alice")
+        bob = JPAKE(secret=secret, signer_id=b"bob")
+
+        bob_one = bob.one()
+
+        self.assertRaises(
+            Exception, alice.process_one,
+            gx3=bob_one['gx1'], gx4=bob_one['gx2']
+        )
+
+    def test_skip_verification_on_dict(self):
+        secret = "hunter42"
+        alice = JPAKE(secret=secret, signer_id=b"alice")
+        bob = JPAKE(secret=secret, signer_id=b"bob")
+
+        self.assertRaises(
+            ValueError, alice.process_one, bob.one(), verify=False
+        )
+
 
 loader = unittest.TestLoader()
 suite = unittest.TestSuite((
