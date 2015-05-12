481f8cefa2b4f27e5bb80f1a134b29c2349a338c
Ben Mather <bwhmather@bwhmather.com>
2015-05-12 11:34:35 +0000

    raise OutOfSequence error when trying to get secret before it is set

--- jpake/__init__.py
+++ jpake/__init__.py
@@ -26,6 +26,8 @@ class JPAKE(object):
 
         :type: int
         """
+        if self.waiting_secret:
+            raise OutOfSequenceError("secret not set")
         return self._secret
 
     def set_secret(self, value):
